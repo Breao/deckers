6aad69199b7e8596bafbb6ab3c33cc8a1139de2c
Breao <breao@hotmail.com>
2022-09-22 19:03:31 -1000

    Updates test file

--- scripts/test.js
+++ scripts/test.js
@@ -2,12 +2,12 @@ const { Deck } = require("../index");
 
 const deck = new Deck();
 
-const deck_test = {
+const deckTest = {
   run: {
-    shuffle: 0,
-    draw: 0,
+    shuffle: 1,
+    draw: 1,
     return: 1,
-  }
+  },
 };
 
 console.log("cards found", deck.cards.length);
@@ -15,7 +15,7 @@ console.log("cards found", deck.cards.length);
 console.log("===== deck =====");
 console.log(deck.cards);
 
-if (deck_test.run.shuffle) {
+if (deckTest.run.shuffle) {
   try {
     deck.shuffle("test");
   } catch (e) {
@@ -24,45 +24,45 @@ if (deck_test.run.shuffle) {
 
   console.log("===== shuffle =====");
   deck.shuffle();
-  deck_test["shuffle"] = { sum_total: 0 };
+  deckTest["shuffle"] = { sum_total: 0 };
   console.log(deck.cards);
   for (let x = 0; x < deck.cards.length; x++) {
-    if (deck_test.shuffle[deck.cards[x].suit] == undefined) {
-      deck_test.shuffle[deck.cards[x].suit] = 0;
+    if (deckTest.shuffle[deck.cards[x].suit] == undefined) {
+      deckTest.shuffle[deck.cards[x].suit] = 0;
     }
-    deck_test.shuffle[deck.cards[x].suit]++;
-    deck_test.shuffle["sum_total"]++;
+    deckTest.shuffle[deck.cards[x].suit]++;
+    deckTest.shuffle["sum_total"]++;
   }
 
-  console.log("shuffle", deck_test.shuffle);
+  console.log("shuffle", deckTest.shuffle);
 
   console.log("===== shuffle hand =====");
-  deck_test["shuffle_hand"] = [
+  deckTest["shuffle_hand"] = [
     { card: 1 },
     { card: 2 },
     { card: 3 },
     { card: 4 },
     { card: 5 },
   ];
-  deck_test.shuffle_hand = deck.shuffle("fy", deck_test.shuffle_hand);
-  console.log(deck_test.shuffle_hand);
+  deckTest.shuffle_hand = deck.shuffle("fy", deckTest.shuffle_hand);
+  console.log(deckTest.shuffle_hand);
 }
 
-if (deck_test.run.draw) {
+if (deckTest.run.draw) {
   console.log("===== draw =====");
-  deck_test["draw"] = { amount: 5, overflow: 3 };
-  for (let x = 0; x < (deck_test.draw.amount + deck_test.draw.overflow); x++) {
+  deckTest["draw"] = { amount: 5, overflow: 3 };
+  for (let x = 0; x < (deckTest.draw.amount + deckTest.draw.overflow); x++) {
     console.log(deck.cards[x]);
   }
   const drawn_cards = deck.drawCards(5);
   console.log("drawn_cards", drawn_cards);
 
-  for (let x = 0; x < deck_test.draw.overflow; x++) {
+  for (let x = 0; x < deckTest.draw.overflow; x++) {
     console.log(deck.cards[x]);
   }
 
   console.log("===== random draw =====");
-  deck_test["randDraw"] = { amount: 5, overflow: 3 };
+  deckTest["randDraw"] = { amount: 5, overflow: 3 };
   console.log("cards before draw:", deck.cards.length);
 
   const rand_drawn_cards = deck.drawRandCards(5);
@@ -71,41 +71,26 @@ if (deck_test.run.draw) {
   console.log("cards after draw:", deck.cards.length);
 }
 
-if (deck_test.run.return) {
+if (deckTest.run.return) {
   console.log("===== return to deck =====");
-  deck_test["return"] = {
+  deckTest["return"] = {
     top: [{ test: "topval" }], topCheck: 4,
     bot: [{ test: "botval" }], botCheck: 4,
-    rand: [{ test: "rand" }]
+    rand: [{ test: "rand" }],
   };
-  deck.returnToDeck(deck_test.return.top, "onTop");
-  for (let x = 0; x < deck_test.return.topCheck; x++) {
+  deck.returnToDeck(deckTest.return.top, "onTop");
+  for (let x = 0; x < deckTest.return.topCheck; x++) {
     console.log(deck.cards[x]);
   }
 
-  deck.returnToDeck(deck_test.return.bot, "onBot");
-  for (let x = (deck.cards.length - deck_test.return.botCheck);
+  deck.returnToDeck(deckTest.return.bot, "onBot");
+  for (let x = (deck.cards.length - deckTest.return.botCheck);
     x < deck.cards.length; x++) {
     console.log(deck.cards[x]);
   }
 
   console.log("Count of cards", deck.cards.length);
-  deck.returnToDeck(deck_test.return.rand, "shuffle");
+  deck.returnToDeck(deckTest.return.rand, "shuffle");
   console.log(deck.cards);
   console.log("Count of cards after random", deck.cards.length);
 }
-
-
-// const max = 5;
-// let result = 1;
-// let counter = 0;
-// while (result != max) {
-//   // result = Math.floor(Math.random() * (max)) + 1;
-//   result = Math.floor(Math.random() * (len - 1) + 1) - 1;
-//   counter++;
-//   console.log(counter, result);
-//   if (counter == 10000) {
-//     result = max;
-//     console.log("failed");
-//   }
-// }
